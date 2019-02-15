a537fbfa363771fbe4cc99031b1a10cc923f57ca
shekhar-patil <patilshekhar900@gmail.com>
2019-02-15 14:27:56 +0530

    Updates

---
+++ precache-manifest.d5e77caed92ea8fd848cc8f01acf0c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sum-target/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a9b6dd60b46a101671af",
+    "url": "/sum-target/static/js/runtime~main.a9b6dd60.js"
+  },
+  {
+    "revision": "d4f08042a3bbd739aa7f",
+    "url": "/sum-target/static/js/main.d4f08042.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/sum-target/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "d4f08042a3bbd739aa7f",
+    "url": "/sum-target/static/css/main.06afc3a0.chunk.css"
+  },
+  {
+    "revision": "b3bfdcb34b10a401ac84b0d73824fcfb",
+    "url": "/sum-target/index.html"
+  }
+];
